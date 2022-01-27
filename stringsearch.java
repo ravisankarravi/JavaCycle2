fd0df822940793f2e96467d036af2c35e6bc1ace
ravisankarravi <77284840+ravisankarravi@users.noreply.github.com>
2022-01-27 23:16:27 +0530

    Add files via upload

---
+++ stringsearch.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class stringsearch
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string");
+		String a = sc.nextLine();
+		int len = a.length();
+		int i,flag=0;
+        System.out.println("Enter the character to be searched");
+		char s = sc.next().charAt(0);
+		for(i=0; i<len ;i++)
+		{
+			if(a.charAt(i)==s)
+			{
+				System.out.println("element " + s + " is present at "+ (i+1));
+				flag=-1;
+				
+				
+			}
+			
+		}
+		if(flag==0)
+		    	System.out.println("element not found");
+
+	}
+}
