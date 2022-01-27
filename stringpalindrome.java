cd35bd2c86108d662e866e01bdcf9c87ffef77bc
ravisankarravi <77284840+ravisankarravi@users.noreply.github.com>
2022-01-27 23:09:46 +0530

    Add files via upload

---
+++ stringpalindrome.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class stringpalindrome
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string");
+		String a = sc.nextLine();
+		int len = a.length();
+		int i,j,flag=0;
+		for(i= len -1,j=0; i>=0&&j<len ;i--,j++)
+			{
+	  			if(a.charAt(i)!=a.charAt(j))
+					{
+						flag = -1;
+						break;
+					}
+			}
+		if(flag==-1)
+			{
+				System.out.println("Not palidrome");	
+			}
+			else
+				System.out.println("Palindrome");
+	}
+}
